4cb65850f8445330799e7ab9e797375473bdc407
papsdroid.fr <32851720+papsdroidfr@users.noreply.github.com>
2020-09-17 21:46:19 +0200

    Add files via upload

---
+++ tests/test_meteo_api.py
@@ -0,0 +1,166 @@
+#!/usr/bin/python3
+
+# usage:  python3 test_api_meteo.py code_postal
+# le token de l'api doit etre dans le fichier tokenAPI.txt
+# exple:  python3 test_api_meteo.py 78570
+
+from contextlib import closing
+from urllib.request import urlopen
+import json
+import I2C_LCD_DRIVER
+import sys
+
+
+# initial LCD devbice with I2C backpack
+lcd = I2C_LCD_DRIVER.lcd()
+
+lcd.lcd_display_string('test API METEO')
+
+#recuperation token API
+try:
+    with open('tokenAPI.txt','r') as f:
+        mytoken= f.read()
+    #print('token="' + mytoken + '"')
+except:
+    print('Créer un fichier tokenAPI.txt avec le token en 1ere ligne')
+    exit(0)
+
+#récuparetation CP dans paramètres de commande
+if len(sys.argv)==1:
+    print('préciser un CP (Code Postal)')
+    print('usage: python3 test_meteo_api.py CP')
+    exit(0)
+
+CP=sys.argv[1]
+
+URLAPI =    'https://api.meteo-concept.com/api'
+GETCITIES = '/location/cities'
+TOKEN =     '?token='+mytoken
+SEARCH =    '&search='+CP
+
+#get city object from API
+with closing(urlopen(URLAPI+GETCITIES+TOKEN+SEARCH)) as f:
+     cities = json.loads(f.read())['cities']
+     print('Il y a {} villes correspondant à la recherche'.format(len(cities)))
+     for city in cities:
+         print('\t{} (code insee:{})'.format(city['name'], city['insee']))
+
+#get unique city CODE INSEE
+print('Quel code INSEE souhaitez vous retenir?')
+INSEE = input('-->  ')
+
+
+
+#Get Forecast Daily meteo
+GETFORECAST='/forecast/daily/0'
+SEARCH = '&insee='+INSEE
+
+#dictionnaire des codes météo fourni par l'api
+WEATHER = {
+	0: "Soleil",
+	1: "Peu nuageux",
+	2: "Ciel voilé",
+	3: "Nuageux",
+	4: "Très nuageux",
+	5: "Couvert",
+	6: "Brouillard",
+	7: "Brouillard givrant",
+	10: "Pluie faible",
+	11: "Pluie modérée",
+	12: "Pluie forte",
+	13: "Pluie faible verglaçante",
+	14: "Pluie modérée verglaçante",
+	15: "Pluie forte verglaçante",
+	16: "Bruine",
+	20: "Neige faible",
+	21: "Neige modérée",
+	22: "Neige forte",
+	30: "Pluie et neige mêlées faibles",
+	31: "Pluie et neige mêlées modérées",
+	32: "Pluie et neige mêlées fortes",
+	40: "Averses de pluie locales et faibles",
+	41: "Averses de pluie locales",
+	42: "Averses locales et fortes",
+	43: "Averses de pluie faibles",
+	44: "Averses de pluie",
+	45: "Averses de pluie fortes",
+	46: "Averses de pluie faibles et fréquentes",
+	47: "Averses de pluie fréquentes",
+	48: "Averses de pluie fortes et fréquentes",
+	60: "Averses de neige localisées et faibles",
+	61: "Averses de neige localisées",
+	62: "Averses de neige localisées et fortes",
+	63: "Averses de neige faibles",
+	64: "Averses de neige",
+	65: "Averses de neige fortes",
+	66: "Averses de neige faibles et fréquentes",
+	67: "Averses de neige fréquentes",
+	68: "Averses de neige fortes et fréquentes",
+	70: "Averses de pluie et neige mêlées localisées et faibles",
+	71: "Averses de pluie et neige mêlées localisées",
+	72: "Averses de pluie et neige mêlées localisées et fortes",
+	73: "Averses de pluie et neige mêlées faibles",
+	74: "Averses de pluie et neige mêlées",
+	75: "Averses de pluie et neige mêlées fortes",
+	76: "Averses de pluie et neige mêlées faibles et nombreuses",
+	77: "Averses de pluie et neige mêlées fréquentes",
+	78: "Averses de pluie et neige mêlées fortes et fréquentes",
+	100: "Orages faibles et locaux",
+	101: "Orages locaux",
+	102: "Orages fort et locaux",
+	103: "Orages faibles",
+	104: "Orages",
+	105: "Orages forts",
+	106: "Orages faibles et fréquents",
+	107: "Orages fréquents",
+	108: "Orages forts et fréquents",
+	120: "Orages faibles et locaux de neige ou grésil",
+	121: "Orages locaux de neige ou grésil",
+	122: "Orages locaux de neige ou grésil",
+	123: "Orages faibles de neige ou grésil",
+	124: "Orages de neige ou grésil",
+	125: "Orages de neige ou grésil",
+	126: "Orages faibles et fréquents de neige ou grésil",
+	127: "Orages fréquents de neige ou grésil",
+	128: "Orages fréquents de neige ou grésil",
+	130: "Orages faibles et locaux de pluie et neige mêlées ou grésil",
+	131: "Orages locaux de pluie et neige mêlées ou grésil",
+	132: "Orages fort et locaux de pluie et neige mêlées ou grésil",
+	133: "Orages faibles de pluie et neige mêlées ou grésil",
+	134: "Orages de pluie et neige mêlées ou grésil",
+	135: "Orages forts de pluie et neige mêlées ou grésil",
+	136: "Orages faibles et fréquents de pluie et neige mêlées ou grésil",
+	137: "Orages fréquents de pluie et neige mêlées ou grésil",
+	138: "Orages forts et fréquents de pluie et neige mêlées ou grésil",
+	140: "Pluies orageuses",
+	141: "Pluie et neige mêlées à caractère orageux",
+	142: "Neige à caractère orageux",
+	210: "Pluie faible intermittente",
+	211: "Pluie modérée intermittente",
+	212: "Pluie forte intermittente",
+	220: "Neige faible intermittente",
+	221: "Neige modérée intermittente",
+	222: "Neige forte intermittente",
+	230: "Pluie et neige mêlées",
+	231: "Pluie et neige mêlées",
+	232: "Pluie et neige mêlées",
+	235: "Averses de grêle",
+}
+
+with closing(urlopen(URLAPI+GETFORECAST+TOKEN+SEARCH)) as f:
+    decoded = json.loads(f.read())
+    (city,forecast) = (decoded[k] for k in ('city','forecast'))
+    print('city', city['name'])
+    print('météo:', WEATHER[forecast['weather']])
+    print('tmin,tmax = {}°C,{}°C'.format(forecast['tmin'], forecast['tmax']))
+    print('probabilité pluie', forecast['probarain'])
+    print('precipitation min,max = {}mm, {}mm'.format(forecast['rr10'],forecast['rr1']))
+
+#get forecast hourly next 12h 
+GETHOURS='/forecast/nextHours'
+HOURLY='&hourly=true'
+with closing(urlopen(URLAPI+GETHOURS+TOKEN+SEARCH)) as f:
+    forecast = json.loads(f.read())['forecast']
+    for n in range(1,len(forecast)):
+    	print('T° dans {}h: {}°C, {}'.format(n,forecast[n]['tsoil2'], WEATHER[forecast[n]['weather']]))
+
